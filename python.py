016b089eeea529eb74c9ee90ddc14cf2b22d608a
Erik Telepovský <erik.telepovsky@gmail.com>
2018-09-04 15:01:36 +0200

    python.py

---
+++ python.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name='django-whisper',
+    version='0.1.0',
+    description='Chat with rooms for Django',
+    long_description=open('README.md').read(),
+    author='Pragmatic Mates',
+    author_email='info@pragmaticmates.com',
+    maintainer='Pragmatic Mates',
+    maintainer_email='info@pragmaticmates.com',
+    url='https://github.com/PragmaticMates/django-whisper',
+    packages=[
+        'whisper',
+        'whisper.migrations'
+    ],
+    include_package_data=True,
+    install_requires=('django',),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Development Status :: 3 - Alpha'
+    ],
+    license='BSD License',
+    keywords="django chat room",
+)
