353e106d58f3bc666934bc0f9680aa0fb81cd84a
Erik Telepovský <erik.telepovsky@gmail.com>
2018-09-04 15:03:10 +0200

    email notifications using django_rq is optional

--- whisper/apps.py
+++ whisper/apps.py
@@ -15,11 +15,21 @@ class ChatConfig(AppConfig):
 
     def schedule_jobs(self):
         print('Scheduling chat jobs...')
-        scheduler = django_rq.get_scheduler('cron')
 
-        # Cron task to check status of companies
-        scheduler.cron(
-            "*/5 * * * *",  # Run every 5 minutes
-            func=notify_about_unread_messages,
-            timeout=settings.RQ_QUEUES['cron']['DEFAULT_TIMEOUT']
-        )
+        try:
+            import django_rq
+            scheduler = django_rq.get_scheduler('cron')
+
+            # Cron task to check status of companies
+            scheduler.cron(
+                "*/5 * * * *",  # Run every 5 minutes
+                func=notify_about_unread_messages,
+                timeout=settings.RQ_QUEUES['cron']['DEFAULT_TIMEOUT']
+            )
+        except ImportError:
+            # django RQ couldn't be imported, the email notifications won't work
+            pass
+        except KeyError:
+            # 'cron' not found in RQ queues
+            # TODO: create whistle setting for this
+            pass
