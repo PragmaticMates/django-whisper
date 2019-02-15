d06ba10aed0d5c2082f997b15913d4c224cad098
Erik Telepovský <erik.telepovsky@gmail.com>
2019-02-15 09:43:57 +0100

    v0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-whisper',
-    version='0.1.0',
+    version='0.2.0',
     description='Chat with rooms for Django based on websockets',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
