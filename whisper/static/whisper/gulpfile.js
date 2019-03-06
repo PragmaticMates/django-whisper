0fd1a007ca999e9c3c05629f869a685839f0894b
rsoltys <radovan.soltys@gmail.com>
2019-03-06 11:17:55 +0100

    update gulp version

--- whisper/static/whisper/gulpfile.js
+++ whisper/static/whisper/gulpfile.js
@@ -4,14 +4,15 @@ var gulp = require( './node_modules/gulp' );
 var sass = require( './node_modules/gulp-sass' );
 var sourcemaps = require('./node_modules/gulp-sourcemaps');
 
-gulp.task('compile', function() {
+gulp.task('compile', function(done) {
 	gulp.src( './scss/chat.scss' )
         .pipe( sourcemaps.init() )
 		.pipe( sass() )
         .pipe( sourcemaps.write( './' ) )
 		.pipe( gulp.dest( './css/' ) );
+	done();
 });
 
 gulp.task('watch', function() {
-	gulp.watch( './scss/chat.scss', ['compile'] );
+	gulp.watch( './scss/chat.scss', gulp.series('compile') );
 });
